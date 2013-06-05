e49500bcb4093fc1f3695b21656c3fb297101bf0
Alessandro Sena <veneinzuela@gmail.com>
2013-06-05 00:45:02 -0300

    Fixing some bugs

--- TweetSearch.py
+++ TweetSearch.py
@@ -8,11 +8,20 @@ import utils
 class TweetSearch():
 
     def __init__(self):
+        # Inverted list index
         self.index = defaultdict(list)
+
+        # Document frequency of each word
         self.df = defaultdict(int)
+
+        # Number of Tweets in the index
         self.N = 0
+
+        # IDF of each word
         self.idf = defaultdict(float)
-        self.norm = defaultdict(float)
+
+        # Norm of each tweet
+        self.norms = defaultdict(float)
 
     def index_collection(self, collection_dir):
         cwd = os.getcwd()
@@ -29,31 +38,30 @@ class TweetSearch():
                 try:
                     if utils.filter_tweet(tweet):
                         self.index_tweet(tweet)
-                        break
                 except Exception, error:
                     print error
             fin.close()
         os.chdir(cwd)
 
+        # Update the idf and norms
         self.update_idf()
         self.update_norms()
+        print "indexed"
 
 
     def index_tweet(self, tweet):
         tid = tweet['id']
         text = tweet['text']
-        #print text
         counter = defaultdict(int)
 
         tokens = utils.tokenize_tweet(text)
-        #print tokens
+    
         for token in tokens:
             counter[token] += 1
-        #print counter
 
         for word, freq in counter.iteritems():
-            freq = 1.0 + math.log(freq, 2)
-            self.index[word].append((tid, freq))
+            tf = 1.0 + math.log(freq, 2)
+            self.index[word].append((tid, tf))
             self.df[word] += 1
 
         self.N += 1
@@ -61,7 +69,7 @@ class TweetSearch():
     def update_idf(self):
         N = float(self.N)
         for word, df in self.df.iteritems():
-            self.idf[word] = math.log(1+(N/float(df)), 2)
+            self.idf[word] = math.log((N/float(df)), 2)
 
     def update_norms(self):
         # Cleans the norm vector 
@@ -78,43 +86,49 @@ class TweetSearch():
         self.norms = norms        
 
 
-    def search(self, query, ranking='vector'):
+    def search(self, query):
 
         accumulator = defaultdict(float)
         counter = defaultdict(int)
         tokens = utils.tokenize_tweet(query)
-
+        
         for token in tokens:
             counter[token] += 1
 
-        query_norm = math.sqrt(sum([(tf*self.idf[word])**2 for word, tf in counter.iteritems()]))
+        # Calculates the query norm
+        query_norm = 0.0 
+        for word, freq in counter.iteritems():
+            tf = 1.0 + math.log(freq, 2)            
+            query_norm += (tf*self.idf[word])**2
+            counter[word] = tf
+        query_norm = math.sqrt(query_norm)
         print query_norm
+
 
         for word, qtf in counter.iteritems():
             for tid, dtf in self.index[word]:
                 accumulator[tid] += (self.idf[word]**2)*qtf*dtf
 
-        scores = [ (tid, value/query_norm*self.norms[tid]) for tid, value in accumulator.iteritems()] 
+        scores = [ (tid, value/(query_norm*self.norms[tid])) for tid, value in accumulator.iteritems()] 
 
         sorted(scores, key=lambda tup: tup[1])[::-1]
 
         return scores
 
 
-
 def main():
     ts = TweetSearch()
-    #ts.index_collection('stream')
-    ts.index_tweet({'id': 1, 'text': "To do is to be To be is to do"})
-    ts.index_tweet({'id': 2, 'text': "To be or not to be I am what I am"})
-    ts.index_tweet({'id': 3, 'text': "I think therefore I am Do be do be do"})
-    ts.index_tweet({'id': 4, 'text': "Do do do da da da Let it be Let it be"})
-    ts.update_idf()
-    ts.update_norms()
-    print ts.idf
-    print ts.norms
-    #print ts.search("Also Angelina Jolie can \"challenge\" society's views on the woman's body because her body already made her millions of dollars! #notsorry")
-
+    ts.index_collection('stream')
+    #ts.index_tweet({'id': 1, 'text': "To do is to be To be is to do"})
+    #ts.index_tweet({'id': 2, 'text': "To be or not to be I am what I am"})
+    #ts.index_tweet({'id': 3, 'text': "I think therefore I am Do be do be do"})
+    #ts.index_tweet({'id': 4, 'text': "Do do do da da da Let it be Let it be"}
+    #ts.update_idf()
+    #ts.update_norms()
+    #print ts.idf
+    #print ts.norms
+    print ts.search("Also Angelina Jolie can \"challenge\" society's views on the woman's body because her body already made her millions of dollars! #notsorry")[:10]
+    print ts.norms[334687426675085312]
 if __name__ == "__main__":
     main()    
 
